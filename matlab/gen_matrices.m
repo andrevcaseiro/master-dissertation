313e4de1aec80cad71c5baaced54074bb4f89eac
andrevcaseiro <andre.caseiro02@gmail.com>
2025-03-05 14:33:38 +0000

    Add matlab files

---
+++ matlab/gen_matrices.m
@@ -0,0 +1,46 @@
+function export_sparse_mat(A, filename)
+    % Get the nonzero elements and their indices
+    [rows, cols, values] = find(A);
+    
+    % Open the file for writing
+    f = fopen(filename, 'w');
+    
+    % Write number of nonzero elements
+    fprintf(f, '%d\n', nnz(A));
+    
+    % Write triplets
+    for i = 1:length(values)
+        fprintf(f, '%d,%d,%g\n', rows(i), cols(i), values(i));
+    end
+    
+    % Close the file
+    fclose(f);
+end
+
+
+for num_dims = 1:3
+    dim = 2^i
+    final_dim = dim^num_dims
+    while final_dim <= 1048576
+        % [dim], [dim dim], [dim dim dim]
+        dims = repmat(dim, 1, num_dims);
+        % {'DD'}, {'DD', 'DD'}, {'DD', 'DD', 'DD'}
+        bcs = repmat({'DD'}, 1, num_dims);
+
+        % Compute the Laplacian matrix
+        [~, ~, A] = laplacian(dims, bcs);
+        A = -A;
+        A_exp = expm(A);
+
+        % Generate filenames
+        laplacian_filename = sprintf('laplacian_%dd_%d.csv', num_dims, final_dim);
+        exp_filename = sprintf('laplacian_%dd_%d_exp.csv', num_dims, final_dim);
+    
+        % Save matrices
+        export_sparse_mat(A, laplacian_filename);
+        export_sparse_mat(A_exp, exp_filename);
+
+        dim = dim * 2
+        final_dim = dim^num_dims
+    end
+end
