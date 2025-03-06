cadfccec42d10cf505573650dde2aae0e42ada54
André Caseiro <andre.v.caseiro@tecnico.ulisboa.pt>
2025-03-06 15:43:00 +0000

    Add generated laplacian matrices

--- matlab/gen_matrices.m
+++ matlab/gen_matrices.m
@@ -1,3 +1,33 @@
+for num_dims = 2:2
+    dim = 256;
+    final_dim = dim^num_dims
+    while final_dim <= 32768
+        % [dim], [dim dim], [dim dim dim]
+        dims = repmat(dim, 1, num_dims);
+        % {'DD'}, {'DD', 'DD'}, {'DD', 'DD', 'DD'}
+        bcs = repmat({'DD'}, 1, num_dims);
+
+        % Compute the Laplacian matrix
+        [~, ~, A] = laplacian(dims, bcs);
+        A = -A;
+        A_exp = expm(A);
+
+        first_row_exp = A_exp(1, :);
+
+        % Generate filenames
+        laplacian_filename = sprintf('laplacian_%dd_%d.csv', num_dims, final_dim);
+        exp_filename = sprintf('laplacian_%dd_%d_exp.csv', num_dims, final_dim);
+    
+        % Save matrices
+        export_sparse_mat(A, laplacian_filename);
+        export_sparse_mat(first_row_exp, exp_filename);
+
+        dim = dim * 2;
+        final_dim = dim^num_dims
+    end
+end
+
+
 function export_sparse_mat(A, filename)
     % Get the nonzero elements and their indices
     [rows, cols, values] = find(A);
@@ -15,32 +45,4 @@ function export_sparse_mat(A, filename)
     
     % Close the file
     fclose(f);
-end
-
-
-for num_dims = 1:3
-    dim = 2^i
-    final_dim = dim^num_dims
-    while final_dim <= 1048576
-        % [dim], [dim dim], [dim dim dim]
-        dims = repmat(dim, 1, num_dims);
-        % {'DD'}, {'DD', 'DD'}, {'DD', 'DD', 'DD'}
-        bcs = repmat({'DD'}, 1, num_dims);
-
-        % Compute the Laplacian matrix
-        [~, ~, A] = laplacian(dims, bcs);
-        A = -A;
-        A_exp = expm(A);
-
-        % Generate filenames
-        laplacian_filename = sprintf('laplacian_%dd_%d.csv', num_dims, final_dim);
-        exp_filename = sprintf('laplacian_%dd_%d_exp.csv', num_dims, final_dim);
-    
-        % Save matrices
-        export_sparse_mat(A, laplacian_filename);
-        export_sparse_mat(A_exp, exp_filename);
-
-        dim = dim * 2
-        final_dim = dim^num_dims
-    end
 end
